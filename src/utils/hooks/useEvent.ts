7e5aa6768cc381dc6f92ebf1dd8b32a4b8f20926
inker <anton.veselyev@gmail.com>
2019-09-22 14:59:56 +0100

    update useEvent

--- src/utils/hooks/useEvent.ts
+++ src/utils/hooks/useEvent.ts
@@ -1,18 +1,9 @@
 import { useEffect } from 'react'
 
-type Handler = (event?: any) => void
-
-export interface UseEventTarget {
-  addEventListener(name: string, handler: Handler, ...args: any[]): void,
-  removeEventListener(name: string, handler: Handler): void,
-}
-
-const defaultTarget = typeof window === 'object' ? window : null
-
 export default (
   name: string,
-  handler?: null | undefined | Handler,
-  target: null | Node | Window = defaultTarget,
+  handler?: null | undefined | EventListenerOrEventListenerObject,
+  target: null | Node | Window = window,
   options?: AddEventListenerOptions,
 ) => {
   useEffect(() => {
@@ -20,9 +11,9 @@ export default (
       return
     }
 
-    target!.addEventListener(name, handler, options)
+    target.addEventListener(name, handler, options)
     return () => {
-      target!.removeEventListener(name, handler)
+      target.removeEventListener(name, handler)
     }
   }, [name, handler, target, JSON.stringify(options)])
 }
