71a80d27b677612ed32502a43243ffa4ea5e25dd
inker <anton.veselyev@gmail.com>
2022-02-18 16:31:06 +0000

    Deploying to gh-pages from  @ c53ab477535659e5fec56ed71d21cf52c6f32e9b 🚀

--- version.b955ab86ba0f90bd1ecd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunksnake=self.webpackChunksnake||[]).push([[872],{5213:()=>{"use strict";const e=document.createElement("meta");e.name="modification-date",e.content="Fri, 18 Feb 2022 16:30:19 GMT",document.head.appendChild(e)}}]);
